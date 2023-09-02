6f0f8ac43031642aab419381093ff36562ce5c7d
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-09-02 04:39:39 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,233 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main() {
-  
+  runApp(MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "App 1",
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: Text("Day 1"),
+//             ),
+//             body: Text("Hello World")
+//         )
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "App 1",
+//       home: Scaffold(
+//         backgroundColor: Colors.grey,
+//         appBar: AppBar(
+//          title: Text("Day 1"),
+//           leading: Icon(Icons.home),
+//           backgroundColor: Colors.black,
+//         ),
+//         body: Center(
+//           child: Text(
+//             "Hello World"
+//           )
+//         )
+//       )
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "App 1",
+//         home: Scaffold(
+//             backgroundColor: Colors.grey,
+//             appBar: AppBar(
+//               title: Text("Day 1"),
+//               leading: Icon(Icons.home),
+//               backgroundColor: Colors.black,
+//             ),
+//             body: Container(
+//                 height: 100,
+//                 width: 100,
+//                 margin: EdgeInsets.all(20),
+//                 padding: EdgeInsets.all(20),
+//                 alignment: Alignment.center,
+//                 color: Colors.orangeAccent,
+//                 child: Text(
+//                     "Hello World"
+//                 )
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "App 1",
+//         home: Scaffold(
+//             backgroundColor: Colors.grey,
+//             appBar: AppBar(
+//               title: Text("Day 1"),
+//               leading: Icon(Icons.home),
+//               backgroundColor: Colors.black,
+//             ),
+//             body: Container(
+//               height: 100,
+//               width: 100,
+//               margin: EdgeInsets.all(20),
+//               padding: EdgeInsets.all(20),
+//               alignment: Alignment.center,
+//               child: Text(
+//                   "Hello World"
+//               ),
+//               decoration: BoxDecoration(
+//                   border: Border.all(width: 4, color: Colors.black),
+//                   color: Colors.orangeAccent,
+//                   borderRadius: BorderRadius.circular(10),
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color: Colors.orange, offset: Offset(6.0, 6.1)
+//                     )
+//                   ]
+//               ),
+//               transform: Matrix4.rotationZ(0.1),
+//               // constraints: BoxConstraints.expand(height: 200)
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "App 1",
+//         home: Scaffold(
+//             backgroundColor: Colors.grey,
+//             appBar: AppBar(
+//               title: Text("Day 1"),
+//               leading: Icon(Icons.home),
+//               backgroundColor: Colors.black,
+//             ),
+//             body: Row(
+//               children: <Widget>[
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("CodeWarriors"),
+//                 ),
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("Hey"),
+//                 ),
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("Coders!!"),
+//                 )
+//               ],
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "App 1",
+//         home: Scaffold(
+//             backgroundColor: Colors.grey,
+//             appBar: AppBar(
+//               title: Text("Day 1"),
+//               leading: Icon(Icons.home),
+//               backgroundColor: Colors.black,
+//             ),
+//             body: Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: <Widget>[
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("CodeWarriors"),
+//                 ),
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("Hey"),
+//                 ),
+//                 Container(
+//                   height: 50,
+//                   width: 100,
+//                   margin: EdgeInsets.all(10),
+//                   color: Colors.orangeAccent,
+//                   child: Text("Coders!!"),
+//                 )
+//               ],
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "App 1",
+        home: Scaffold(
+            backgroundColor: Colors.grey,
+            appBar: AppBar(
+              title: Text("Day 1"),
+              leading: Icon(Icons.home),
+              backgroundColor: Colors.black,
+            ),
+            body: Text(
+                "Hello!! Coders, You are here with this new event based on Flutter!!!",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w700,
+                  backgroundColor: Colors.amberAccent
+                ),
+            )
+        )
+    );
+  }
+}
+
+
+
