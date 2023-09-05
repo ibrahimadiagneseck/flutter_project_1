11da0adf2488f301da89c361e519df792c2cfed6
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-09-05 08:13:05 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,6 @@ void main() {
 //   }
 // }
 
-
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -109,9 +108,6 @@ void main() {
 //     );
 //   }
 // }
-
-
-
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -154,9 +150,6 @@ void main() {
 //     );
 //   }
 // }
-
-
-
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -201,7 +194,6 @@ void main() {
 //   }
 // }
 
-
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -230,7 +222,6 @@ void main() {
 //     );
 //   }
 // }
-
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -268,8 +259,6 @@ void main() {
 //     );
 //   }
 // }
-
-
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -319,8 +308,6 @@ void main() {
 //     );
 //   }
 // }
-
-
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -388,7 +375,7 @@ void main() {
 // }
 
 
-
+// ********************************************************************
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -457,9 +444,10 @@ void main() {
 //     );
 //   }
 // }
+// ********************************************************************
 
 
-
+// ********************************************************************
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -587,7 +575,11 @@ void main() {
 //     );
 //   }
 // }
+// ********************************************************************
 
+
+
+// ********************************************************************
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -691,7 +683,10 @@ void main() {
 //             )));
 //   }
 // }
+// ********************************************************************
 
+
+// ********************************************************************
 // class MyApp extends StatelessWidget {
 //
 //   @override
@@ -725,7 +720,10 @@ void main() {
 //     );
 //   }
 // }
+// ********************************************************************
 
+
+// ********************************************************************
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -795,18 +793,409 @@ void main() {
 //             )));
 //   }
 // }
+// ********************************************************************
+
+
+
+// ********************************************************************
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "My App",
+//         home: Scaffold(
+//             appBar: AppBar(title: Text("Forms")),
+//             body: Forms()
+//         )
+//     );
+//   }
+// }
+//
+// class Forms extends StatefulWidget {
+//   @override
+//   _FormsState createState() => _FormsState();
+// }
+//
+//
+// class _FormsState extends State<Forms> {
+//
+//   final keys = GlobalKey<FormState>();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Form(
+//       key: keys,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           TextFormField(
+//               decoration: InputDecoration(
+//                   hintText: "Enter your name",
+//                   labelText: "Name",
+//                   icon: Icon(Icons.person)
+//               ),
+//             validator: (value) {
+//                 if(value != null && value.isEmpty) {
+//                   return 'Please fill the the details';
+//                 }
+//                 return null;
+//             }
+//           ),
+//           TextFormField(
+//               decoration: InputDecoration(
+//                   hintText: "Enter Password",
+//                   labelText: "Password",
+//                   icon: Icon(Icons.lock)
+//               ),
+//               validator: (value) {
+//                 if(value != null && value.isEmpty) {
+//                   return 'Please fill the the details';
+//                 }
+//                 return null;
+//               }
+//           ),
+//           TextFormField(
+//               decoration: InputDecoration(
+//                   hintText: "Github link",
+//                   labelText: "Github link",
+//                   icon: Icon(Icons.link)
+//               ),
+//               validator: (value) {
+//                 if(value != null && value.isEmpty) {
+//                   return 'Please fill the the details';
+//                 }
+//                 return null;
+//               }
+//           ),
+//           Container(
+//             child: TextButton(
+//                     child: Text(
+//                       "Submit",
+//                       style: TextStyle(fontSize: 20),
+//                     ),
+//                     onPressed: () {
+//                       if(keys.currentState!.validate()) {
+//                         ScaffoldMessenger.of(context).showSnackBar(
+//                           const SnackBar(content: Text('Processing Data')),
+//                         );
+//                       }
+//                     },
+//                     style: TextButton.styleFrom(
+//                       foregroundColor: Colors.white,
+//                       backgroundColor: Colors.lightBlueAccent
+//                     ),
+//                   ),
+//           )
+//         ]
+//       )
+//     );
+//   }
+// }
+// ********************************************************************
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "My App",
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 title: Text("Icons Widget"),
+//                 backgroundColor: Colors.black
+//             ),
+//             body: Center(
+//               child: Padding(
+//                 padding: const EdgeInsets.all(28.0),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     Icon(
+//                       Icons.home,
+//                       color: Colors.black,
+//                       size: 40,
+//                       textDirection: TextDirection.rtl
+//                     ),
+//                     SizedBox(
+//                       height: 40,
+//                     ),
+//                     Icon(
+//                         Icons.access_alarm,
+//                         color: Colors.black,
+//                         size: 40,
+//                         textDirection: TextDirection.rtl
+//                     ),
+//                     SizedBox(
+//                       height: 40,
+//                     ),
+//                     Icon(
+//                         Icons.delete,
+//                         color: Colors.black,
+//                         size: 40,
+//                         textDirection: TextDirection.rtl
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "My App",
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 title: Text("Icons Widget"),
+//                 backgroundColor: Colors.black
+//             ),
+//             body: Padding(
+//               padding: const EdgeInsets.all(28.0),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.home,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Home")
+//                     ],
+//                   ),
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.access_alarm,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Alarm")
+//                     ],
+//                   ),
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.delete,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Delete")
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "My App",
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 title: Text("Icons Widget"),
+//                 backgroundColor: Colors.black
+//             ),
+//             body: Padding(
+//               padding: const EdgeInsets.all(28.0),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.home,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Home")
+//                     ],
+//                   ),
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.access_alarm,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Alarm")
+//                     ],
+//                   ),
+//                   Column(
+//                     children: [
+//                       Icon(
+//                           Icons.delete,
+//                           color: Colors.black,
+//                           size: 40,
+//                           textDirection: TextDirection.rtl
+//                       ),
+//                       Text("Delete")
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             )
+//         )
+//     );
+//   }
+// }
+
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "My App",
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 title: Text("Image Widget"),
+//                 backgroundColor: Colors.black
+//             ),
+//             body: Padding(
+//               padding: const EdgeInsets.all(28.0),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   Column(
+//                     children: [
+//                       Image.asset(
+//                           "assets/joker.jpeg",
+//                           width: 200
+//                       ),
+//                       Text("Why So Serious???"),
+//                       Image.network(
+//                         "https://th.bing.com/th/id/OIP.xRA_m1gxfh8_aCtBHxwYpwHaEr?w=277&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+//                         width: 200
+//                       ),
+//                       Container(
+//                         child: FadeInImage.assetNetwork(
+//                           placeholder: "assets/lion.jpeg",
+//                           image: "https://th.bing.com/th/id/OIP.xRA_m1gxfh8_aCtBHxwYpwHaEr?w=277&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+//                           width: 200,
+//                           height: 200
+//                         )
+//                       )
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             )
+//         )
+//     );
+//   }
+// }
+
 
 
 
 class MyApp extends StatelessWidget {
-
-  final
-
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My App",
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Image Widget"),
+                backgroundColor: Colors.black
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Center(
+                child: Container(
+                  height: 200,
+                  width: 300,
+                  child: Card(
+                    color: Colors.yellow,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
+                    ),
+                    elevation: 10,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: ListTile(
+                            leading: Icon(
+                                Icons.person,
+                                size: 40,
+                                color: Colors.blue,
+                            ),
+                            title: Text(
+                              "Ibrahima Diagne Seck",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black
+                              ),
+                            ),
+                            subtitle: Text(
+                              "COO",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                fontSize: 16
+                              )
+                            ),
+                          ),
+                        ),
+                        ButtonBar(
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.download,
+                                size: 24.0,
+                              ),
+                              label: Text('Download'),
+                            ),
+                            TextButton.icon(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.lightBlueAccent
+                              ),
+                              icon: Icon(
+                                Icons.download,
+                                size: 24.0,
+                              ),
+                              label: Text('Download'),
+                            ),
+                            // OutlinedButton.icon(
+                            //   onPressed: () {},
+                            //   icon: Icon(
+                            //     Icons.download,
+                            //     size: 24.0,
+                            //   ),
+                            //   label: Text('Download'),
+                            // )
+                          ],
+                        ),
 
-    return Container();
+                      ],
+                    ),
+                  )
+                ),
+              ),
+            )
+        )
+    );
   }
 }
-
-
